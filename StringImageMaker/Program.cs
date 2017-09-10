471bb930939612e900cd557ee0be49caeaef582c
Tomoaki Oiki <laket72@gmail.com>
2017-09-10 13:48:33 +0900

    first commit

--- StringImageMaker/Program.cs
+++ StringImageMaker/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StringImageMaker.StringDrawing;
 
 namespace StringImageMaker
 {
@@ -10,6 +12,32 @@ namespace StringImageMaker
     {
         static void Main(string[] args)
         {
+            FontManager fontManager = new FontManager(
+                    new string[] { "meiryo UI"},
+                    minSize: 14,
+                    maxSize: 14
+                );
+            IMessageCreator messageCreator =  RandomCharactorCreator.makeNumericCreator(minLen_: 3, maxLen_: 7);
+
+            var drawer = new StringDrawer(messageCreator, fontManager);
+
+            /*
+            CharacterRange[] ranges = new CharacterRange[]{new CharacterRange(0, targetString.Length)};
+
+            format.SetMeasurableCharacterRanges(ranges);
+
+            Region[] regions = g.MeasureCharacterRanges(targetString, font, new RectangleF(x: 0, y: 0, width:256, height:128), format);
+            
+            foreach (var r in regions)
+            {
+                g.DrawRectangle(Pens.Blue, Rectangle.Round(r.GetBounds(g)));
+            }
+            */
+            for (int i = 0; i < 10; i++)
+            {
+                var bitmap = drawer.drawNext();
+                bitmap.Save(String.Format("sample{0}.png", i));
+            }
         }
     }
 }
---
+++ StringImageMaker/StringDrawing/StringDrawer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringImageMaker.StringDrawing
+{
+    /// <summary>
+    /// 文字列を描画するクラス
+    /// </summary>
+    class StringDrawer
+    {
+        public StringDrawer(IMessageCreator messageCreator, FontManager fontManager)
+        {
+            messageCreator_ = messageCreator;
+            fontManager_ = fontManager;
+            sizeCalculator_ = new FontSizeCalculator();
+        }
+
+        /// <summary>
+        /// 文字列を描画する
+        /// 毎回異なる文字列を描画する
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap drawNext() {
+            var curFont = fontManager_.nextFont();
+            var curMessage = messageCreator_.nextString();
+
+            SizeF sizef = sizeCalculator_.getSize(curFont, curMessage);
+            Size imageSize = Size.Round(sizef);
+
+            Bitmap canvas = new Bitmap(width: imageSize.Width, height: imageSize.Height);            
+            Graphics g = Graphics.FromImage(canvas);
+            g.Clear(Color.White);
+            g.DrawString(curMessage, curFont, Brushes.Black, x: 0, y: 0, format:StringFormat.GenericTypographic);
+
+            g.Dispose();
+
+            return canvas;
+        }
+
+        IMessageCreator messageCreator_;
+        FontManager fontManager_;
+        FontSizeCalculator sizeCalculator_;
+    }
+}
