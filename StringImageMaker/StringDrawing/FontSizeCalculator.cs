66b12c074afbd2f073eea9c2737b1d4648fe0cce
Tomoaki Oiki <laket72@gmail.com>
2017-09-10 14:08:01 +0900

    change size

--- StringImageMaker/StringDrawing/FontSizeCalculator.cs
+++ StringImageMaker/StringDrawing/FontSizeCalculator.cs
@@ -15,7 +15,7 @@ namespace StringImageMaker.StringDrawing
         public FontSizeCalculator()
         {
             // MEMO: このBitmapのサイズ影響を調べる
-            canvas_ = new Bitmap(width: 256, height: 128);
+            canvas_ = new Bitmap(width: 512, height: 128);
             graphics_ = Graphics.FromImage(canvas_);
             //graphics_.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
             StringFormat format = new StringFormat();
